cc596094b891d2d80fd7f51ded04f8c75c27d77d
mel2oo <l977631253@gmail.com>
2025-07-17 14:58:45 +0800

    add otel gin

---
+++ otelgin/version.go
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package otelgin
+
+// Version is the current release version of the gin instrumentation.
+func Version() string {
+	return "0.62.0"
+}
